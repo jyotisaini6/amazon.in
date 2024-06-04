30cf0f6806cbc547571515b3c67494a3f230c731
jyotisaini6 <sainijyoti6876@gmail.com>
2024-06-04 17:41:13 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let slideBtnLeft = document.getElementById("slide-btn-left")
+let slideBtnRight = document.getElementById("slide-btn-right")
+
+slideBtnLeft.addEventListener("click",()=>{
+   alert("left btn")
+})
+slideBtnRight.addEventListener("click",()=>{
+   alert("right btn")
+})
